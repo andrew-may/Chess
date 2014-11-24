8cc4a25b8af6b56ea2764efb7fc8e0411b03d1e7
andrew-may <andrew.may@ryerson.ca>
2014-11-24 01:47:38 -0500

    init

---
+++ Chess/ChessPictureBox.vb
@@ -0,0 +1,12 @@
+﻿Public Class ChessPictureBox
+    Inherits PictureBox
+    Private _HasImage As Boolean = False
+    Public Property HasImage() As Object
+        Get
+            Return _HasImage
+        End Get
+        Set(ByVal value As Object)
+            _HasImage = value
+        End Set
+    End Property
+End Class
